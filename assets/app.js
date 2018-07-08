c66955145a45a09af7b49b4ecc78864d8351cbb9
Alek Angelov <alek.angelov.sk@gmail.com>
2018-07-08 19:09:58 +0200

    Arrow theme switching
    Added arrow theme switching and you check the checkbox while doing it because why not

--- assets/app.js
+++ assets/app.js
@@ -1,18 +1,32 @@
 var checkBox = document.getElementById("theme");
 
+
 var theme = {
     themeChange: function () {
         var linkNode = document.getElementById('linktheme');
-        var mainTheme = 'assets/main.css';
-        var darkTheme = 'assets/dark.css';
+        var theme = {
+            "mainTheme": "assets/main.css",
+            "darkTheme": "assets/dark.css",
+        }
             if (checkBox.checked == true) {
-                linkNode.href=darkTheme;
+                linkNode.href=theme['darkTheme'];
                 console.log("now it's dark");
             } else {
-                linkNode.href=mainTheme;
+                linkNode.href=theme['mainTheme'];
                 console.log("now it's light")
             }
+    },
+};
+
+document.onkeyup = function checkBoxSwitch(e) {
+    if (e.which == 37 || e.which == 39) {
+        if (checkBox.checked == true) {
+            checkBox.checked = false;
+        } else if (checkBox.checked == false) {
+            checkBox.checked = true;
+        }
     }
-}
+    theme.themeChange();
+};
 
 checkBox.onclick = theme.themeChange;
